db77e385562adfdb672a8754b31b97c5bed7d62f
Mohamed Samy <Mohamed.Mohamed054@eng-st.cu.edu.eg>
2024-12-19 23:14:54 +0200

    Revert "cell + add beltaction + nady"
    This reverts commit af9a571b6fa83b1d756348026a0e66a846703269.

--- Cell.cpp
+++ Cell.cpp
@@ -1,5 +1,5 @@
 #include "Cell.h"
-#include "Flag.h"
+
 #include "Grid.h"
 #include "GameObject.h"
 #include "Belt.h"
@@ -52,23 +52,24 @@ Flag * Cell::HasFlag() const
 {
 
 	///TODO: Implement the following function like HasBelt() function
-	//done
-	return dynamic_cast<Flag*>(pGameObject); // THIS LINE SHOULD CHANGED WITH YOUR IMPLEMENTATION
+
+	return false; // THIS LINE SHOULD CHANGED WITH YOUR IMPLEMENTATION
+
 }
 WaterPit * Cell::HasWaterPit() const
 {
 
 	///TODO: Implement the following function like HasBelt() function
-	//done
-	return dynamic_cast<WaterPit*>(pGameObject); // THIS LINE SHOULD CHANGED WITH YOUR IMPLEMENTATION
+
+	return false; // THIS LINE SHOULD CHANGED WITH YOUR IMPLEMENTATION
 
 }
 
 DangerZone * Cell::HasDangerZone() const
 {
 	///TODO: Implement the following function like HasBelt() function
-	//done
-	return dynamic_cast<DangerZone*>(pGameObject);; // THIS LINE SHOULD CHANGED WITH YOUR IMPLEMENTATION
+
+	return false; // THIS LINE SHOULD CHANGED WITH YOUR IMPLEMENTATION
 }
 
 
@@ -83,25 +84,13 @@ void Cell::DrawCellOrWaterPitOrDangerZone(Output* pOut) const
 		pOut->DrawCell(position,UI.CellColor); // draw empty cell 
 }
 
-
-
-
-
-
-
-
-
-
-//TODO
 // separate from the above function 
 //because other game objects should be drawn AFTER All Cells are drawn
 //because other game objects don't change color of the cell
 void Cell::DrawGameObject(Output* pOut) const
 {
-	////////TODO: edit this incomplete implemntation to check for other game objects (excluding waterpits and dangerzones)
-	if ((HasFlag()|| HasBelt()) && (!HasDangerZone() && !HasWaterPit()))
+	//TODO: edit this incomplete implemntation to check for other game objects (excluding waterpits and dangerzones)
+	if (HasFlag()|| HasBelt())
 		pGameObject->Draw(pOut); // draw game object
-	else
-		pOut->DrawCell(position, UI.CellColor);
 
 }
