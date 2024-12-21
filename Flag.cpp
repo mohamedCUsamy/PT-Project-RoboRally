17ad17c32d4f2e18444adc5fed0efa6f8a2027a2
Nady <mohamed.amr.nes@gmail.com>
2024-12-21 22:07:31 +0200

    Samy: AddFlagAction & Flag

--- Flag.cpp
+++ Flag.cpp
@@ -15,9 +15,12 @@ void Flag::Apply(Grid *pGrid, Player *pPlayer)
 	// == Here are some guideline steps (numbered below) to implement this function ==
 
 	// 1- Print a message "You have reached a flag. Click to continue ..." and wait mouse click
+	Output* pOut;
+	pOut->PrintMessage("You have reached a flag. Click to continue ...");
 
 	// 2- Apply the flag's effect by ending the game
 	//    Review the "pGrid" functions and decide which function can be used for that
+	pGrid->SetEndGame(true);
 }
 
 Flag::~Flag()
